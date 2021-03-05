922ef9197521144fea217896e3f09dcc76b89a1e
Marcus <ratrateroo@gmail.com>
2021-03-05 20:06:37 +0800

    added queue and queue with linked list

---
+++ 2 Lists and Tables/3 Building a Queue/building-a-queue.js
@@ -0,0 +1,38 @@
+class Queue {
+	constructor() {
+		this.items = [];
+	}
+
+	enqueue(value) {
+		this.items.unshift(value);
+	}
+
+	dequeue() {
+		return this.items.pop();
+	}
+
+	isEmpty() {
+		return this.items.length === 0;
+	}
+
+	toArray() {
+		return this.items.slice();
+	}
+}
+
+const queue = new Queue();
+
+queue.enqueue('Max');
+queue.enqueue('Manu');
+queue.enqueue('Julie');
+
+console.log(queue.toArray());
+
+console.log(queue.dequeue());
+
+console.log(queue.toArray());
+
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+
+console.log(queue.toArray());
