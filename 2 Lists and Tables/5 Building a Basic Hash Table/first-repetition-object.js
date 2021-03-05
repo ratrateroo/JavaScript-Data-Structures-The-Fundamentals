e663936cb4bef0f332ab03f03ef6c87247d4f80c
Marcus <ratrateroo@gmail.com>
2021-03-05 22:25:32 +0800

    added basic hash table

---
+++ 2 Lists and Tables/5 Building a Basic Hash Table/first-repetition-object.js
@@ -0,0 +1,15 @@
+const word = 'hello';
+
+function findFirstRep(str) {
+	const table = {};
+	for (const char of str) {
+		if (table[char]) {
+			return char;
+		}
+		table[char] = 1;
+	}
+}
+
+// Time Complexity: O(n)
+
+console.log(findFirstRep(word));
